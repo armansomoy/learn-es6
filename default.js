d5742b9aaac3ab685e371353edf474f3bafed897
armansomoy <armansamoy42@gmail.com>
2020-08-16 11:08:38 +0600

    second commit

---
+++ default.js
@@ -0,0 +1,7 @@
+function add(num1, num2 = 5 ){
+
+    return num1 + num2;
+}
+
+const result = add(15);
+console.log(result);
---
+++ letconst.js
@@ -0,0 +1,33 @@
+// const name = 'Arman Hossain Somoy';
+// console.log(name);
+
+// const ages = [15, 20, 25, 30];
+// ages[1] = 30;
+// ages.push(35);
+// console.log(ages);
+
+
+
+
+
+
+// let name = 'Arman';
+// name = 'arif';
+// console.log(name);
+// let numbers = 0;
+// for(let i = 0; i < 10; i++){
+//     numbers = numbers + i;
+// }
+// console.log(i);
+// Output =  i is not defined
+
+
+let name = 'Arman';
+name = 'arif';
+console.log(name);
+let numbers = 0;
+for(var i = 0; i < 10; i++){
+    numbers = numbers + i;
+}
+console.log(i);
+// Output =  arif 10 
